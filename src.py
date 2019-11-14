7bd06c6d399635ef4342f0ed9347f8089767a925
smbh <smb_h@yahoo.com>
2019-11-14 13:10:07 +0330

    extracting data

---
+++ src.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+# response = requests.get('https://fa.wikipedia.org/wiki/%D8%AF%D8%A7%D9%86%D8%B4%DA%AF%D8%A7%D9%87_%D8%A2%D8%B2%D8%A7%D8%AF_%D8%A7%D8%B3%D9%84%D8%A7%D9%85%DB%8C_%D9%88%D8%A7%D8%AD%D8%AF_%DA%A9%D8%B1%D8%AC')
+# print(response.status_code)
+# print(response.content)
+# soup = BeautifulSoup(response.text, 'html.parser')
+# print(soup.p)
